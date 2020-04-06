0d48ec074c17ce599fcddc01d3414f74f0f7a481
Irenee14 <irenegabardacortes@gmail.com>
2020-04-06 23:22:54 +0200

    Version 3.0

--- soccerGame2/app/src/main/java/com/al376538/soccergame/model/managers/SquadManager.kt
+++ soccerGame2/app/src/main/java/com/al376538/soccergame/model/managers/SquadManager.kt
@@ -12,6 +12,11 @@ import java.util.jar.Attributes
 class SquadManager(private var model: Model) {
 
     private var arrayPlayer = ArrayList<SquadPlayer>()
+    private var arraygoalkeeper = ArrayList<SquadPlayer>()
+    private var arrayDefender = ArrayList<SquadPlayer>()
+    private var arrayMidfielder = ArrayList<SquadPlayer>()
+    private var arrayAttacker = ArrayList<SquadPlayer>()
+
     private lateinit var coach : SquadPlayer
 
     fun collectSquadPlayer(idteam : Int, listener: Response.Listener<ArrayList<SquadPlayer>>) {
@@ -29,10 +34,8 @@ class SquadManager(private var model: Model) {
     }
 
     fun parseSquad(response: JSONObject, listener: Response.Listener<ArrayList<SquadPlayer>>) {
-
-
         try{
-
+            var arraySave = ArrayList<SquadPlayer>()
             val arraySquad = response.getJSONArray("squad")
 
             for(i in 0 until arraySquad.length()) {
@@ -53,8 +56,9 @@ class SquadManager(private var model: Model) {
                     coach = player
                 }
                 else
-                    arrayPlayer.add(player)
+                    arraySave.add(player)
             }
+            setArray(arraySave)
             listener.onResponse(arrayPlayer)
         }catch (e : JSONException) {
             Log.d("MSK", "Error JSON")
@@ -92,5 +96,23 @@ class SquadManager(private var model: Model) {
     }
     fun getNumber(player: SquadPlayer) : String {
         return player.shirtNumber
+    }
+
+    private fun setArray(array: ArrayList<SquadPlayer>) {
+        for(i in 0 until array.size){
+            if(array[i].position == "Goalkeeper")
+                arraygoalkeeper.add(array[i])
+
+            if(array[i].position == "Defender")
+                arrayDefender.add(array[i])
+            if(array[i].position == "Midfielder")
+                arrayMidfielder.add(array[i])
+            if(array[i].position == "Attacker")
+                arrayAttacker.add(array[i])
+        }
+        arrayPlayer.addAll(arraygoalkeeper)
+        arrayPlayer.addAll(arrayDefender)
+        arrayPlayer.addAll(arrayMidfielder)
+        arrayPlayer.addAll(arrayAttacker)
     }
 }
